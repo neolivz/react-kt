186d6f6758eb3cd1b48b1559c4f61b8db0cf42e5
Jishnu Viswanath <github@jishnu.me>
2018-03-20 13:57:46 +1100

    Initial Version Of Server

---
+++ server/server.js
@@ -0,0 +1,69 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const cors = require('koa2-cors')
+const bodyParser = require('koa-bodyparser')
+
+const app = new Koa()
+const router = new Router()
+
+const userList =  {}
+const users  = {
+	list: async (ctx, next) => {
+		ctx.body = userList
+		next()
+	},
+	create: async (ctx, next) => {
+		console.log(ctx.request.body)
+		const body = ctx.request.body
+		const {userId, password} = body
+		if(users[userId] === undefined) {
+			users[userId] = password
+			ctx.body = {
+				userId,
+				message: 'User Successfully Created'
+			}
+		} else {
+			ctx.status = 400
+			ctx.body ={
+				userId,
+				message: 'User already exist'
+			}
+		}
+	},
+	login: async (ctx, next) => {
+		
+		const body = ctx.request.body
+		const {userId, password} = body
+		console.log(userId, password)
+		if(!users[userId] || users[userId] !== password){
+			ctx.status = 401
+			ctx.body = {
+				userId,
+				message: 'Username or password invalid'
+			}
+		} else {
+			ctx.status = 200
+			ctx.body = {
+				userId,
+				message: 'Successfully login'
+			}
+		}
+		next()
+	}
+}
+
+router
+	.get('/users/', users.list)
+	.post('/login/', users.login)
+	.post('/create/', users.create)
+
+app
+	.use(cors())
+	.use(bodyParser())
+	.use(router.routes())
+	.use(router.allowedMethods())
+
+app.listen(4000)
+	console.log('listening on port 4000')
+
+
